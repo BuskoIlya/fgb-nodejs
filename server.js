fbcb8f5f88dceaa9862c4d01b87a4aad66eff676
Busko Ilya <trassae95st@mail.ru>
2022-12-11 14:39:33 +0300

    Create contacks and ranks api

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const cors = require('cors');
 
 const tournaments = require("./src/tournaments.js");
 const players = require("./src/players.js");
+const contacts = require("./src/contacts.js");
+const ranks = require("./src/ranks.js");
 
 const app = express();
 app.use(cors());
 
-app.get(process.env.API_TOURNAMENTS, function (req, res) {res.json(tournaments)});
-app.get(process.env.API_PLAYERS, function (req, res) {res.json(players)});
+app.get(process.env.API_TOURNAMENTS, function (req, res) { res.json(tournaments) });
+app.get(process.env.API_PLAYERS, function (req, res) { res.json(players) });
+app.get(process.env.API_CONTACTS, function (req, res) { res.json(contacts) });
+app.get(process.env.API_RANKS, function (req, res) { res.json(ranks) });
 
 app.listen(process.env.PORT);
