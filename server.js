cd158a8af9520952fa5ea5614d456e2268c257e0
Busko Ilya <trassae95st@mail.ru>
2022-12-17 12:13:52 +0300

    Create wagc data

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const players = require("./src/players.js");
 const contacts = require("./src/contacts.js");
 const ranks = require("./src/ranks.js");
 const news = require("./src/news.js");
+const worldChampionships = require("./src/world_championships.js");
 
 const app = express();
 app.use(cors());
@@ -18,5 +19,6 @@ app.get(process.env.API_PLAYERS, function (req, res) { res.json(players) });
 app.get(process.env.API_CONTACTS, function (req, res) { res.json(contacts) });
 app.get(process.env.API_RANKS, function (req, res) { res.json(ranks) });
 app.get(process.env.API_NEWS, function (req, res) { res.json(news) });
+app.get(process.env.API_WORLD_CHAMPIONSHIPS, function (req, res) { res.json(worldChampionships) });
 
 app.listen(process.env.PORT);
